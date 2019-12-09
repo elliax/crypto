c5a06ad068d024280df4c5ed5854a0a662bb774d
Ellinor Axelsson Vaughn <ellinorav@gmail.com>
2019-12-09 08:49:08 +0100

    css update

--- src/components/CryptoTabs/CryptoTabs.jsx
+++ src/components/CryptoTabs/CryptoTabs.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 import './tabs.css';
 import {NavLink} from 'react-router-dom';
 
