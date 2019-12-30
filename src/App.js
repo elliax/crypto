d2706f615d5a233387bd5987006bee60cb4c80f0
Ellinor Axelsson Vaughn <ellinorav@gmail.com>
2019-12-30 15:06:25 +0100

    test

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ class App extends Component {
     return (  
     <Router>
       <div className="App">
-        <Menu/>
+        <Menu/> 
         <Switch>
         <Route path="/" exact component={Home}/>
         <Route path="/about" component={About}/>
